0a1bfc9f24b6894831f166dde10e041eaad4adc2
Lena <girafffffmc@gmail.com>
2024-01-30 14:26:26 +0700

    Добавьте файлы проекта.

---
+++ Pages/Authoriz.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _22_106_Ochapova_MoneyInPocket.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Authoriz.xaml
+    /// </summary>
+    public partial class Authoriz : Window
+    {
+        public Authoriz()
+        {
+            InitializeComponent();
+
+            
+
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            //SqlCommand command = new SqlCommand(sqlExpression, connection);
+            //SqlDataReader reader = command.ExecuteReader();
+
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=22-106-Ochapova-MoneyInEveryPocket;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                string sqlExpression = "Select * from dbo.[User] Where Login = '" + tbLogin.Text + "' and Password = '" + PassBox.Password + "'";// + tbPassword + "";
+                //string sqlExpression = "Select IDUser from dbo.[User] Where Login = 11198151 and Password = '3QjGHM'";
+                connection.Open();
+                string query = $"SELECT * FROM [User] WHERE Login = '{tbLogin.Text}' AND password = '{PassBox.Password}'";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                string login = reader.GetString(1);
+                                string password = reader.GetString(2);
+                                if (password == PassBox.Password)
+                                {
+                                    UserPage userPage = new Pages.UserPage(reader);
+                                    userPage.Show();
+                                }
+                            }
+                        }
+                        connection.Close();
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Pages/UserPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _22_106_Ochapova_MoneyInPocket.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для UserPage.xaml
+    /// </summary>
+    public partial class UserPage : Window
+    {
+        public UserPage(SqlDataReader reader)
+        {
+            InitializeComponent();
+            
+            tbl_fio.Text =  reader.GetString(3) + " " +reader.GetString(4) +" "+ reader.GetString(5);
+            tbPhone.Text = reader.GetString(8);
+            tbAdress.Text = reader.GetString(9);
+            tbEmail.Text = reader.GetString(10);
+            DateTime Date = reader.GetDateTime(13);
+            tbBirth.Text = Date.ToString();
+            tbBirthPlace.Text = reader.GetString(14);
+        }
+    }
+}
